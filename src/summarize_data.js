04241d6286a8cd59858d9e6d70c707fd0250793d
Jim Vallandingham <vlandham@gmail.com>
2015-03-02 21:38:58 -0800

    summarize

--- src/summarize_data.js
+++ src/summarize_data.js
@@ -45,11 +45,62 @@ console.log(JSON.stringify(large_land));
 // ```
 
 //
-// ## Max/Min
+// ## Min & Max
 //
-// ## Average
+// With `forEach` it would be possible to laboriously calculate metrics such as min, and max. But, its typically better to work smart, not hard.
+//
+// As it turns out, D3 comes to the rescue again, with [d3.min](https://github.com/mbostock/d3/wiki/Arrays#d3_min) and [d3.max](https://github.com/mbostock/d3/wiki/Arrays#d3_max). Use the callback function to indicate which property (or computed value based on the properties) to access.
+
+var minLand = d3.min(data, function(d) { return d.land_area; });
+console.log(minLand);
+// ```
+//=> 48.3
+// ```
+
+var maxLand = d3.max(data, function(d) { return d.land_area; });
+console.log(maxLand);
+// ```
+//=> 315
+// ```
+
+// If you want both of them at the same time, you can use [d3.extent](https://github.com/mbostock/d3/wiki/Arrays#d3_extent)
+
+var landExtent = d3.extent(data, function(d) { return d.land_area; });
+console.log(landExtent);
+// ```
+//=> [48.3, 315]
+// ```
+// This returns an array with the first element the minimum value and the second element the maximum.
+//
+// ## Summary Statistics
+//
+// D3 provides a few basic tools to analyze your data, all using the same format as the min and max functions. Simply provide the property you would like to analyze, and you are good to go.
+//
+// [d3.mean](https://github.com/mbostock/d3/wiki/Arrays#d3_mean)
+
+var landAvg = d3.mean(data, function(d) { return d.land_area; });
+console.log(landAvg);
+// ```
+//=> 187.45
+// ```
+// [d3.median](https://github.com/mbostock/d3/wiki/Arrays#d3_median)
+
+var landMed = d3.median(data, function(d) { return d.land_area; });
+console.log(landMed);
+// ```
+//=> 193.25
+// ```
+// [d3.deviation](https://github.com/mbostock/d3/wiki/Arrays#d3_deviation) - for standard deviation
+
+var landSD = d3.deviation(data, function(d) { return d.land_area; });
+console.log(landSD);
+// ```
+//=> 140.96553952414519
+// ```
 //
 // ## Reducing
+//
+// These functions all take an array and reduce it down to a single number. But what if that number isn't the one you want? Well, you can take this reduction into your own hands with `reduce`!
 //
 // ## See Also
 //
