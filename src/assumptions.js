3363d9ead6741e23b6abeb7bd1e8898902416353
Jim Vallandingham <vlandham@gmail.com>
2015-03-18 10:29:16 -0700

    Updates

--- src/assumptions.js
+++ src/assumptions.js
@@ -1,19 +1,3 @@
-// # Checking Data Assumptions
-//
-// Data processing is tricky business, full of pitfalls and gotchas. Hopefully the tasks in this guide help with getting started in this process. But you, I, and the entire world will make mistakes. It's natural.
-//
-// But mistakes in data processing, like all other kinds of mistakes, can be painful. They can result in hours of bug hunting, days of reprocessing, and months of crying. Since we know mistakes happen and will continue to happen, what can we do to take away some of the pain?
-//
-// In a word, _padding_. We need some padding to protect us from the bumps and bruises of data processing. And I would suggest that this padding come in the form of simple tests that check the assumptions you have about the shape and contents of your data.
-//
-// Unless there is an extreme performance need, these tests should run in the data processing pipeline. Optimally, they would be easy to turn on and off so that you can disable them if you need to if your code is deployed.
-//
-// ## Assertions
-//
-// These tests can be created with [assertions](http://en.wikipedia.org/wiki/Assertion_%28software_development%29) - functions that check the truthiness of a statement in code. Typically, they raise an error when an expected truth is not actually true.
-//
-// JavaScript doesn't have a built assertions, but we can rectify this deficiency with a simple function.
-
 function assert(isTrue, message) {
   if(!isTrue) {
     console.log(message);
@@ -22,16 +6,6 @@ function assert(isTrue, message) {
   return true;
 }
 
-// This will output a given message if the input is not true. Typically assertions [throw](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/throw) errors, but we can just log it for explaining purposes.
-//
-// ## Data Content Assumptions
-//
-// Now let's use our `assert` function to check some assumptions about the details of our data.
-//
-// We can use lodash's suite of [type checking functions](https://lodash.com/docs#isBoolean) to take care of performing the checks, passing the result of the check to `assert` to produce our errors.
-//
-// Let's say our data importing process has made some mistakes:
-//
 var data = [{"name":"Dan",
              "age":23,
              "superhuman":false},
@@ -39,10 +13,6 @@ var data = [{"name":"Dan",
               "age":NaN,
               "superhuman":"TRUE"}
 ];
-
-// Our first entry looks ok, where our second entry has some problems. The age parsing for the immortal [Sleepwalker](http://en.wikipedia.org/wiki/Sleepwalker_%28comics%29) has left him with no age. Also, bad input data has left us with a string in `superhuman`, where we expect a boolean.
-//
-// A simple assumption checking function that could be run on this data could look something like this:
 
 function checkDataContent(data) {
   data.forEach(function(d) {
@@ -56,23 +26,6 @@ function checkDataContent(data) {
 
 checkDataContent(data);
 
-//```
-//=> {"name":"Sleepwalker","age":null,"superhuman":"TRUE"} has a bad age - should not be NaN
-//    {"name":"Sleepwalker","age":null,"superhuman":"TRUE"} has a bad superhuman - should be boolean
-//```
-// <div class="aside">This code is using lodash</div>
-//
-//
-// Again, the focus here is on detection of data problems. You want something quick and simple that will serve as an early warning sign.
-//
-// Unfortunately, the JavaScript primitive `NaN` is indeed a number, and so additional checks need to be made. As more data comes in, this function will need to be updated to add more checks. This might get a bit tedious, but a little bit of checking can go a long way towards maintaining sanity.
-//
-// ## Data Shape Assumptions
-//
-// Just as you can test your assumptions about the content of your data elements, it can be a good idea to test your assumptions about the _shape_ of your data. Here, shape just refers to the size and structure of your data. Rows and columns.
-//
-// Something simple to perform this check could look like this:
-
 function checkDataShape(data) {
   assert(data.length > 0, "data is empty");
   assert(data.length > 4, "data is too small");
@@ -81,36 +34,5 @@ function checkDataShape(data) {
 }
 
 checkDataShape(data);
-//```
-//=> data is too small
-//    wrong number of columns
-//```
-// The two assumption functions could easily be combined into one, but it's important to look at both aspects of your data.
-//
-// ## More Assertions
-//
-// If this is an approach that appeals to you, and your data might get really complicated (or really messy)
-// you may want to explore using more complicated assertion code.
-//
-// One useful library to explore is [Chai](http://chaijs.com/api/assert/) which comes with a great
-// collection of assertion helpers. These can help you check for more complicated things like whether
-// two objects are equal or whether an object has or doesn't have a property.
-//
-// For example:
 
 assert.deepEqual({ tea: 'green' }, { tea: 'green' });
-
-// ```
-// => true
-// ```
-//
-// <div class="aside">This code is using chai's assert library</div>
-//
-// ## Next Task
-//
-// [Using Node](node.html)
-//
-// ## See Also
-//
-// - [Parsing raw data](http://www.pgbovine.net/parsing-raw-data.htm) - a great guide that motivated this section
-// - [Chai](http://chaijs.com/api/assert/) - Chai's assert library
--- src/strings.js
+++ src/strings.js
@@ -1,64 +1,15 @@
-// # Working with Strings
-//
-// String cleaning is something you end up doing quite a lot. Hopefully this task will help make the process less painful. There are a near infinite transformations you might want to do with strings, so we won't get to everything, but this will serve as a starting point for common manipulations that will come up again and again.
-//
-// We will start with generic JavaScript string functions and add in a bit of [lodash](https://lodash.com/) magic to make things easier.
-//
-// ## String Basics
-//
-// Similar to arrays, the characters in strings are accessible via indexing
-//
 var aChar = "Hello There!"[6];
 console.log(aChar);
-// ```
-//=> T
-//```
-//
-// Also, just like arrays, you have access to the powerful [slice](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/slice) method, which is used to extract sub-sections based on indexes.
+
 var aSlice = "Hello There!".slice(6,11);
 console.log(aSlice);
-// ```
-//=> There
-//```
-// The sliced string goes up to - but not including - the last index.
-//
-// And, of course, string concatenation is done in JavaScript using the `+` operator. Use parenthesis if you want to do actual arithmetic inside your concatenation.
 
 var orderNum = 8;
 console.log("You are number " + (orderNum + 1) + " in line.");
-// ```
-//=> You are number 9 in line.
-//```
-//
-// Check [the documentation](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String) for all the other basic tools.
-//
-// ## Stripping Whitespace
-//
-// Often, you are going to have some surrounding whitespace that you don't want corrupting the rest of your data. Reading CSV files gives a good example of this, as spaces are typically also used in conjunction with the commas to separate columns.
-//
-// A data file like this:
-// ```
-// cities_spaced.csv:
-//
-// city  ,state ,population,land area
-//   seattle  ,WA , 652405 ,83.9
-// new york,NY,8405837,  302.6
-// ```
-// When [read in](read_data.html) can produce quite the messy dataset:
 
 d3.csv("data/cities_spaced.csv", function(data) {
   console.log(JSON.stringify(data));
 });
-
-// ```
-// => [{"city  ":"  seattle  ","state ":"WA ","population":" 652405 ","land area":"83.9   "},
-//      {"city  ":"new york","state ":"NY","population":"8405837","land area":"  302.6"}]
-//```
-// <div class="aside">This code is using d3.js</div>
-//
-// Note the spaces in the property names as well as the values. In cases like this, it might be best to [map](iterate_data.html) the data back to a clean version. Lodash's [trim](https://lodash.com/docs#trim) can help. It removes that unsightly whitespace from the front and back of your strings.
-//
-// Here is a version of the data loading function that removes whitespace. It uses
 
 d3.csv("data/cities_spaced.csv", function(data) {
   var clean = data.map(function(d) {
@@ -70,95 +21,27 @@ d3.csv("data/cities_spaced.csv", function(data) {
   });
   console.log(JSON.stringify(clean));
 });
-// ```
-// =>  [{"city":"seattle","state":"WA","population":"652405","land area":"83.9"},
-//       {"city":"new york","state":"NY","population":"8405837","land area":"302.6"}]
-// ```
-// <div class="aside">This code is using d3.js and lodash</div>
-//
-// The strings are now clear of those pesky spaces.
-//
-// ## Find and Replace
-//
-// Extracting data from strings can sometimes mean extracting pieces of strings. Finding out if a string contains a keyword or sub-string of interest is a first step in quantifying the content of a body of text.
-//
-// [indexOf]() can be used to perform this searching. You pass it a sub-string, and it'll tell you the location in string you are calling it where that sub-string starts. `-1` is returned if the sub-string can't be found. You can use this to build a little string finder, by comparing the return value to `-1`.
-//
+
 console.log("A man, a plan, a canal".indexOf("man") !== -1);
-// ```
-// => true
-// ```
+
 console.log("A man, a plan, a canal".indexOf("panama") !== -1);
-// ```
-// => false
-// ```
-// Replace is the butter to find's bread. We will see more replacing when we get to regular expressions, but replacing sections of a string can be done with the [replace]() method.
-//
+
 console.log("A man, a plan, a canal".replace("canal", ""));
-// ```
-// => "A man, a plan, a"
-// ```
-//
-// ## Templating
-//
-// When you need to create a more complicated string, such as an html snippet, it may
-// become too tedious to just combine strings by concatenating them with your variables. Consider
-// the following example:
-//
-// ```html
-// <div class="person">
-//   <span class="name">Birdman</span>
-//   <span class="occupation">Imaginary Super Hero</span>
-// </div>
-// ```
-// If we wanted to build it using string concatenation, it might look like this:
-//
+
 var person = { name : "Birdman", occupation: "Imaginary Super Hero" };
 var html_snippet = "<div class=\"person\">" +
   "<span class=\"name\">" + person.name + "</span>" +
   "<span class=\"occupation\">" + person.occupation + "</span>" +
 "</div>";
 console.log(html_snippet);
-// ```
-// => '<div class="person"><span class="name">Birdman</span><span class="occupation">Imaginary Super Hero</span></div>'
-// ```
-//
-// That's a lot of string escaping! You can imagine this gets pretty hard to manage
-// after a while.
-//
-// In order to simplify this process, you can use lodash templates to define a "template"
-// that you can reuse with different data. Using our example above, we might define it
-// like so:
-//
+
 var templateString = "<div class='person'>" +
   "  <span class='name'><%= name %></span>" +
   "  <span class='occupation'><%= occupation %></span>" +
   "</div>";
 var templateFunction = _.template(templateString);
 
-// Now you can use this template function with lots of data to generate the
-// same snippet of html:
-
 console.log(templateFunction(person));
-//
-// ```
-// => '<div class="person"><span class="name">Birdman</span><span class="occupation">Imaginary Super Hero</span></div>'
-// ```
-// <div class="aside">This code is using lodash</div>
-//
+
 var anotherPerson = { name : "James. James Bond", occupation: "Spy" };
 console.log(templateFunction(anotherPerson));
-//
-// ```
-// => '<div class="person"><span class="name">James. James Bond</span><span class="occupation">Spy</span></div>'
-// ```
-// 
-// ## Next Task
-//
-// [Regular Expressions](regexes.html)
-//
-// ## See Also
-//
-// - [Working With Strings](http://www.quirksmode.org/js/strings.html) - a great guide to more string basics
-// - [underscore.string](https://github.com/epeli/underscore.string) - for all the other string functions you might want
-// - [underscore.template](http://underscorejs.org/#template) - for a deeper dive into underscore's template function
