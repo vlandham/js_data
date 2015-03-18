bb2b22d569bcf84500170b26a622a7dfeb68815e
Jim Vallandingham <vlandham@gmail.com>
2015-03-18 10:19:57 -0700

    inject for all locales

--- tasks/inject-script-tag.js
+++ tasks/inject-script-tag.js
@@ -1,8 +1,10 @@
+var path = require('path');
+
 module.exports = function(grunt) {
 
   grunt.config.set('inject-script-tag', {
     options: {
-      html_files: 'public/*.html',
+      html_files: ['public/*.html', 'public/**/*.html'],
       js_files: "src/*.js",
     }
   });
@@ -13,10 +15,16 @@ module.exports = function(grunt) {
     var options = this.options();
     var html_files = grunt.file.expand(options.html_files);
     var js_files = grunt.file.expand(options.js_files);
+    var js_map = {};
+    js_files.forEach(function(js_file) {
+      js_map[path.basename(js_file, path.extname(js_file))] = js_file;
+    });
 
 
     html_files.forEach(function(html_file, idx) {
-      var js_file = js_files[idx];
+      var html_id = path.basename(html_file, path.extname(html_file));
+
+      var js_file = js_map[html_id];
 
       var html = grunt.file.read(html_file);
       var js = grunt.file.read(js_file);
