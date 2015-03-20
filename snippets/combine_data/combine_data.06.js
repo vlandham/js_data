ff070e419cc13b18dd0a6cac291064df7473779c
Timo Grossenbacher <wnstnsmth@yahoo.com>
2015-03-20 16:53:28 +0100

    seems like paths to data must be absolute..?

--- snippets/combine_data/combine_data.06.js
+++ snippets/combine_data/combine_data.06.js
@@ -1,7 +1,7 @@
 queue()
-    .defer(d3.csv, "data/big_data_1.csv")
-    .defer(d3.csv, "data/big_data_2.csv")
-    .defer(d3.csv, "data/big_data_3.csv")
+    .defer(d3.csv, "/data/big_data_1.csv")
+    .defer(d3.csv, "/data/big_data_2.csv")
+    .defer(d3.csv, "/data/big_data_3.csv")
     .await(combine);
 
 function combine(error, big_data_1, big_data_2, big_data_3) {
