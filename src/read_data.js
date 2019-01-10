29a49ee02b89c51c6b2c51cc7114e224263f906b
Jim Vallandingham <jvallandingham@zymergen.com>
2019-01-10 14:42:28 -0800

    Updates

--- src/read_data.js
+++ src/read_data.js
@@ -1,8 +1,8 @@
-d3.csv("/data/cities.csv", function(data) {
+d3.csv("/data/cities.csv").then(function(data) {
   console.log(data[0]);
 });
 
-d3.csv("/data/cities.csv", function(data) {
+d3.csv("/data/cities.csv").then(function(data) {
   data.forEach(function(d) {
     d.population = +d.population;
     d["land area"] = +d["land area"];
@@ -17,40 +17,26 @@ d3.csv("/data/cities.csv", function(d) {
     population : +d.population,
     land_area : +d["land area"]
   };
-}, function(data) {
+}).then(function(data) {
   console.log(data[0]);
 });
 
-d3.tsv("/data/animals.tsv", function(data) {
+d3.tsv("/data/animals.tsv").then(function(data) {
   console.log(data[0]);
 });
 
-var psv = d3.dsvFormat("|");
+d3.dsv("|", "/data/animals.txt").then(function(data){
+  console.log(data[1]);
+});
 
-// This parser can parse pipe-delimited text:
-var output = psv.parse("first|last\nabe|lincoln")
-console.log(output[0])
-
-d3.request("/data/animals_piped.txt")
-  .mimeType("text/plain")
-  .response(function(xhr) { return psv.parse(xhr.responseText) })
-  .get(function(data) {
-    console.log("pipe-delimited data:")
-    console.log(data[1]);
-  });
-
-d3.json("/data/employees.json", function(data) {
+d3.json("/data/employees.json").then(function(data) {
   console.log(data[0]);
 });
 
-d3.queue()
-  .defer(d3.csv, "/data/cities.csv")
-  .defer(d3.tsv, "/data/animals.tsv")
-  .await(analyze);
-
-function analyze(error, cities, animals) {
-  if(error) { console.log(error); }
-
-  console.log(cities[0]);
-  console.log(animals[0]);
-}
+Promise.all([
+  d3.csv("/data/cities.csv"),
+  d3.tsv("/data/animals.tsv")
+]).then(function(data) {
+  console.log(data[0][0])  // first row of cities
+  console.log(data[1][0])  // first row of animals
+});
